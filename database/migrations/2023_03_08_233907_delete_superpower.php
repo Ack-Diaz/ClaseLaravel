5084b6e8098a17705d5ac399cdfe954d341b992a
Ack-Diaz <ackerley.diaz@tecmilenio.mx>
2023-03-08 18:06:01 -0600

    Initial commit

---
+++ database/migrations/2023_03_08_233907_delete_superpower.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('table_heros', function (Blueprint $table) {
+            $table->dropColumn('superpower');
+            $table->dropColumn('first_appereance');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('table_heros', function (Blueprint $table) {
+            $table->string('superpower');
+            $table->string('first_appereance');
+        });
+    }
+};
